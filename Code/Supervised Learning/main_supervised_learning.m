8d76a9f33a4a6b2e43322546ad03a567e0e14229
Usman Muhammad <67257724+Usman1021@users.noreply.github.com>
2022-02-01 11:46:15 +0200

    Update main_supervised_learning.m

--- Code/Supervised Learning/main_supervised_learning.m
+++ Code/Supervised Learning/main_supervised_learning.m
@@ -4,6 +4,7 @@
 % Muhammad, Usman, Zitong Yu, and Jukka Komulainen. "Self-supervised 2D face presentation attack detection via temporal sequence sampling." (2021). % 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%% For any problem in running the code, please contact me through following emails: usman@mail.bnu.edu.cn  or muhammad.usman@oulu.fi
 %% Train the Model for Supervised Learning
 % Follow the instructions given by the datasets to split real and fake videos. Make two folders named as “real and “attack”. 
 % Put the videos into these folders and train the model for face anti-spoofing. 
